3906e8c8386d45a3d257b9fe3cd2e55976e6071c
Adi Matok <adimatok01@gmail.com>
2025-05-29 07:59:22 +0300

    עדכון כולל לקבצי הפרויקט

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,7 +6,6 @@ class User(models.Model):
     salt = models.BinaryField()
     password_hash = models.CharField(max_length=64)
 
-    # היסטוריית סיסמאות אחרונות
     previous_password_hash1 = models.CharField(max_length=64, blank=True)
     previous_password_hash2 = models.CharField(max_length=64, blank=True)
     previous_password_hash3 = models.CharField(max_length=64, blank=True)
@@ -17,9 +16,9 @@ class User(models.Model):
 
 class Package(models.Model):
     name = models.CharField(max_length=100)
-    speed = models.CharField(max_length=50)  # לדוגמה: "100 Mbps"
+    speed = models.CharField(max_length=50)  
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    sector = models.CharField(max_length=50)  # לדוגמה: "Business" או "Private"
+    sector = models.CharField(max_length=50)  
 
     def __str__(self):
         return f"{self.name} ({self.speed})"
@@ -30,6 +29,7 @@ class Customer(models.Model):
     last_name = models.CharField(max_length=100)
     id_number = models.CharField(max_length=20, unique=True)
     package = models.ForeignKey(Package, on_delete=models.SET_NULL, null=True, blank=True)
+    created_by = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
